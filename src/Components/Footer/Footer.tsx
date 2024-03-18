5e9dc3e63e6effb78710cbd6cc35624ee1e6a4ab
Curtis Huang <curtisahuang@gmail.com>
2024-03-18 14:27:34 +0000

    Updated components

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,17 +1,21 @@
 import FooterStyles from "./footer.module.css";
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div data-testid="footer-wrapper" className={FooterStyles.footerWrapper}>
-      <div className={FooterStyles.footerContainer}>
-        <p className={FooterStyles.footerCopyright}>© Nokkel</p>
-        <div className={FooterStyles.footerSeparator} />
-        <a href="/support" className={FooterStyles.footerLink}>
-          Help and support
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            data-testid="footer-wrapper"
+            className={FooterStyles.footerWrapper}
+        >
+            <div className={FooterStyles.footerContainer}>
+                <p className={FooterStyles.footerCopyright}>© Nokkel</p>
+                <div className={FooterStyles.footerSeparator} />
+                <a href="/support" className={FooterStyles.footerLink}>
+                    Help and support
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
---
+++ src/Components/IndexCard/index.ts
@@ -0,0 +1,1 @@
+export { default as IndexCard } from "./IndexCard";
