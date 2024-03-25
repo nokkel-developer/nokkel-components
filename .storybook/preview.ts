1ac73d71050cd0ced8705b3f1d28ab429ea66772
Curtis Huang <curtisahuang@gmail.com>
2024-03-25 16:34:33 +0000

    wip storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from "@storybook/react";
+import "../src/styles/global.css";
+import type { ThemeConfig } from "storybook-addon-data-theme-switcher";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const globalTypes = {
+  dataThemes: {
+    defaultValue: {
+      list: [
+        { name: "aviva", dataTheme: "aviva" },
+        { name: "nokkel", dataTheme: "nokkel" },
+        { name: "quilter", dataTheme: "quilter" },
+      ],
+    } satisfies ThemeConfig,
+  },
+};
+
+export default preview;
---
+++ src/components/ImageCard/ImageCard.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ImageCard from "./ImageCard";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "ImageCard",
+  component: ImageCard,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    title: { control: "title" },
+  },
+} satisfies Meta<typeof ImageCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    title: "first story",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    title: "second story",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    title: "thirdstory",
+  },
+};
