8485494c39601e8ac43917bc57e219678015b6f4
Curtis Huang <curtisahuang@gmail.com>
2024-03-28 10:29:11 +0000

    Updated imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Footer } from "./Footer";
 export { default as IndexCard } from "./IndexCard";
 export { default as ImageCard } from "./ImageCard";
 export { default as VideoCard } from "./VideoCard";
+export { default as Header } from "./Header";
